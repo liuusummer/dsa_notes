93c6ad039d7d9b5d2f0fe02c38eef6e9378276ee
SITuser <2545259707@qq.com>
2022-03-09 14:30:44 +0800

    ch02

--- ch2/main.cpp
+++ ch2/main.cpp
@@ -36,56 +36,43 @@ T horner(T coeff[], int n, const T &x) {
 }
 
 
-template <typename T>
-void rank(T a[], int n, T r[]) {
+
+
+template<class T>
+void rank(T a[], int n, int r[]) {
+	// 给数组a[0:n-1]的n个元素排名次  input:4 3 9 3 7   output:2 0 4 1 3
+	// 结果在r[0:n-1]中返回
+
 	for (int i = 0; i < n; i++) {
-		r[i] = 0;  //初始化
+		r[i] = 0; // 初始化
 	}
-	for (int j = 1; j < n; j++) {
-		for (int i = 0; i < j; i++) {
-			if (a[i] <= a[j]) r[j]++;
-			else {
-				r[i]++;
-			}
+	// 比较所有元素对
+
+	for (int i = 1; i < n; i++) {   // 从数组的第二个元素开始
+		for (int j = 0; j < i; j++) {   // 比较它与前面的元素
+			if (a[j] <= a[i]) r[i]++;
+			else    // 如果前面的值<=它  递增它的rank
+				r[j]++;  // 前面的值>它 递增该值的rank
+			// 这样做不会重复比较 只比较了每个元素与它前面的元素
 		}
 	}
 }
 
-//template<class T>
-//void rank(T a[], int n, int r[]) {
-//	// 给数组a[0:n-1]的n个元素排名次  input:4 3 9 3 7   output:2 0 4 1 3
-//	// 结果在r[0:n-1]中返回
-//
-//	for (int i = 0; i < n; i++) {
-//		r[i] = 0; // 初始化
-//	}
-//	// 比较所有元素对
-//
-//	for (int i = 1; i < n; i++) {   // 从数组的第二个元素开始
-//		for (int j = 0; j < i; j++) {   // 比较它与前面的元素
-//			if (a[j] <= a[i]) r[i]++;
-//			else    // 如果前面的值<=它  递增它的rank
-//				r[j]++;  // 前面的值>它 递增该值的rank
-//			// 这样做不会重复比较 只比较了每个元素与它前面的元素
-//		}
-//	}
-//}
-//
-//template <class T>
-//void rearrange(T a[], int n, T r[]) {
-//	// 利用附加数组u[] 根据rank的计数排序 r[]是a[]的ranking
-//	T *u = new T[n];
-//	// 将a中元素赋给u  根据rank对应到各自的位置上
-//	for (int i = 0; i < n; i++) {
-//		u[r[i]] = a[i];
-//	}
-//	for (int i = 0; i < n; i++) {
-//		a[i] = u[i];
-//	}
-//	delete[]u;
-//
-//
-//}
+template <class T>
+void rearrange(T a[], int n, T r[]) {
+	// 利用附加数组u[] 根据rank的计数排序 r[]是a[]的ranking
+	T *u = new T[n];
+	// 将a中元素赋给u  根据rank对应到各自的位置上
+	for (int i = 0; i < n; i++) {
+		u[r[i]] = a[i];
+	}
+	for (int i = 0; i < n; i++) {
+		a[i] = u[i];
+	}
+	delete[]u;
+
+
+}
 
 template<class T>
 int indexOfMax(T a[], int n) {
@@ -104,7 +91,8 @@ int indexOfMax(T a[], int n) {
 }
 template<class T>
 void selectionSort(T a[], int n) {
-	// 选择排序
+	// 选择排序：首先找到最大的元素将其移动到a[n-1]，在剩下的n-1个元素中找出最大的元素
+	// 将它移动到a[n-2]
 	for (int i = n; i > 1; i--) {
 		int index = indexOfMax(a, i);
 		std::swap(a[index], a[i - 1]);
@@ -113,7 +101,8 @@ void selectionSort(T a[], int n) {
 
 template<class T>
 void bubble(T a[], int n) {
-	// 一次冒泡过程
+	// 一次冒泡过程：相邻元素比较，如果左边的大于右边，则交换
+	// 一次冒泡结束后，最大的元素肯定在最右端
 	for (int i = 0; i < n - 1; i++) {
 		if (a[i] > a[i + 1]) {
 			std::swap(a[i], a[i + 1]);
@@ -127,24 +116,26 @@ void bubbleSort_(T a[], int n) {
 		bubble(a, i);
 	}
 }
+
 template <class T>
 void insert(T a[], int &n, const T &x) {
-	// 把x插入到a[0:n-1]
+	// 把x插入到有序数组a[0:n-1]
 	// 假设a的容量大于n
 	int i;
 	for (int i = n - 1; i >= 0 && x < a[i]; i--) {
 		a[i + 1] = a[i];
 	}
-	a[i + 1] = x;
+	a[i + 1] = x;   // 循环结束时插入位置
 	n++; // array 多了一个元素
 }
 
 template<class T>
 void rearrange(T a[], int n, int r[]) {
-	// 原地重排   r[]保存名次
+	// 原地重排   r[]保存的是a[]的rank
 	for (int i = 0; i < n; i++) {
-		while (r[i] != i) {
-			int temp = r[i];
+		while (r[i] != i) {   // 对每个元素，找到rank[i]=i的元素，
+								// 如果不是，就交换数组值和rank值
+			int temp = r[i];    
 			std::swap(a[i], a[temp]);
 			std::swap(r[i], r[temp]);
 		}
@@ -158,7 +149,7 @@ void selectionSort_(T a[], int n) {
 	for (int size = n; (!sorted) && (size > 1); size--) {   // 只在无序情况下进行操作
 		int indexOfMax = 0;
 		sorted = true;
-		for (int j = 1; j < n; j++) {
+		for (int j = 1; j < size; j++) {
 			if (a[indexOfMax] <= a[j]) {
 				indexOfMax = j;    // 说明是有序的：从小到大  sorted不用更改  继续为true
 									// 此轮结束后while直接结束
@@ -167,7 +158,7 @@ void selectionSort_(T a[], int n) {
 				sorted = false;   // 并不有序 sorted改为false
 			}
 		}
-		std::swap(a[indexOfMax], a[size - 1]);
+		std::swap(a[indexOfMax], a[size - 1]);  // 交换最大的和最后一个元素
 
 	}
 }
